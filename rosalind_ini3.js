7cd4dfc9c0944fe51cee6a421d2931165098fe53
rbcasperson <casperson.ryan@gmail.com>
2016-03-05 21:24:37 -0500

    solved ini3

---
+++ rosalind_ini3.js
@@ -0,0 +1,11 @@
+module.exports = {
+    main: function (data) {
+        var lo = require('lodash');
+        var datas = lo.split(data, "\n"),
+            nums = lo.map(lo.split(datas[1], " "), lo.parseInt),
+            text1 = datas[0].slice(nums[0], nums[1] + 1),
+            text2 = datas[0].slice(nums[2], nums[3] + 1);
+            
+        return text1 + " " + text2;
+    }
+};
