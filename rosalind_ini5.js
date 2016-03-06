b13323062589c48822253512152a3e526d478f4c
Droogans <droogans@gmail.com>
2016-03-05 23:00:22 -0600

    refact(ini): Update all to stop using

--- rosalind_ini5.js
+++ rosalind_ini5.js
@@ -1,14 +1,12 @@
-module.exports = {
-    main: function (data) {
-        var lo = require("lodash");
-        var evens = [],
-            lines = lo.split(data, "\n");
-        lines.pop();
-        
-        for (i = 1; i < lines.length; i += 2) {
-            evens.push(lines[i]);
-        };
-        
-        return lo.join(evens, "\n"); 
-    }
-}
+module.exports = function (data) {
+    var lo = require("lodash");
+    var evens = [],
+        lines = lo.split(data, "\n");
+    lines.pop();
+
+    for (i = 1; i < lines.length; i += 2) {
+        evens.push(lines[i]);
+    };
+
+    return lo.join(evens, "\n");
+};
