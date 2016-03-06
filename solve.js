43d8b6212a3f77327e7c741acfab65987711a7b0
Droogans <droogans@gmail.com>
2016-03-05 23:09:46 -0600

    fix(solve): Use string interpolation, not concatenation

--- solve.js
+++ solve.js
@@ -2,15 +2,15 @@ var fs = require('fs');
 var os = require('os');
 
 var args = process.argv.slice(2);
-var problemName = "rosalind_" + args[0];
-var problemSolver = require("./" + problemName);
-var downloadsDirectory = os.homedir() + '/Downloads/';
+var problemName = `rosalind_${args[0]}`;
+var problemSolver = require(`./${problemName}`);
+var downloadsDirectory = `${os.homedir()}/Downloads/`;
 
 if (args[1]) {
-    downloadsDirectory = os.homedir() + args[1] + "/";
+    downloadsDirectory = `${os.homedir()}${args[1]}/`;
 };
 
-var problemFile = downloadsDirectory + problemName + ".txt"
+var problemFile = `${downloadsDirectory}${problemName}.txt`;
 var data = fs.readFileSync(problemFile, {encoding: "utf8"});
 
 console.log(problemSolver(data));
