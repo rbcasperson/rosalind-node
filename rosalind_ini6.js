fe4fd5728657b3fbbfa72061d74fdb417f897246
rbcasperson <casperson.ryan@gmail.com>
2016-03-07 19:37:35 -0500

    use countBy instead of reduce

--- rosalind_ini6.js
+++ rosalind_ini6.js
@@ -2,15 +2,7 @@
 import * as _ from "lodash";
 
 export function main(data) {
-    var count = _.reduce(_.words(data), (count, word) => {
-        if (count[word]) {
-            count[word] += 1
-        } else {
-            count[word] = 1
-        };
-        return count
-    }, {});
-    return _.trim(_.reduce(count, (result, value, key) => {
+    return _.trim(_.reduce(_.countBy(_.words(data)), (result, value, key) => {
         return result + `${key} ${value}\n`;
     }, ""))
 }
